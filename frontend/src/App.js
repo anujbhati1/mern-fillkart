e0c66666356a61403abdab28a916ca81e69a7eb4
Anuj Bhati <anujbhati7488@gmail.com>
2022-06-17 14:50:55 +0530

    Lesson-03-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/" className="italic">
+          Fillkart
+        </a>
+      </header>
+      <main>list product</main>
+    </div>
+  );
+}
+
+export default App;
