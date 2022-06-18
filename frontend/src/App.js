fb9bae1cbd976b92f226ab569eb4da496625309e
Anuj Bhati <anujbhati7488@gmail.com>
2022-06-18 10:51:36 +0530

    Lesson-10-Add-Bootstrap-UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,36 @@
 import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/" className="italic">
-            Fillkart
-          </Link>
+          <Navbar bg="primary" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand className="fst-italic fw-bold">
+                  Fillkart
+                </Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen />} />
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
