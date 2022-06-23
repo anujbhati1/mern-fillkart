e29d55ec3ada0e0e3edada16af6607ed888114b3
Anuj Bhati <anujbhati7488@gmail.com>
2022-06-23 13:48:21 +0530

    Lesson-21-Seed-Sample-Users

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Anuj',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Raja',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       // _id: '1',
